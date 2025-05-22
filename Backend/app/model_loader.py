25ada50f707a2cf726b73e5c2264563673cb3e87
arunpolson <arunpolson19@gmail.com>
2025-05-22 12:59:04 +0200

    Update model_loader.py

--- Backend/app/model_loader.py
+++ Backend/app/model_loader.py
@@ -1,7 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 def load_model(model_path="./sentiment_model"):
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
-    model = AutoModelForSequenceClassification.from_pretrained(model_path)
-    labels = ["Negative", "Slightly Negative", "neutral", "Positive", "Slightly Positive"]
+    tokenizer = AutoTokenizer.from_pretrained('Backend/app/sentiment_model')
+model = AutoModelForSequenceClassification.from_pretrained('Backend/app/sentiment_model')
+ labels = ["Negative", "Slightly Negative", "neutral", "Positive", "Slightly Positive"]
     return tokenizer, model, labels
