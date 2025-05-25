7e8c56ebb0af3331cbc1d9513213aa9a0c75dfd0
arunpolson <arunpolson19@gmail.com>
2025-05-25 11:18:54 +0200

    Update __init__.py

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -1,1 +1,8 @@
-# Makes 'app' a Python package (can be left empty)
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+def load_model():
+    analyzer = SentimentIntensityAnalyzer()
+    labels = ["Negative", "Neutral", "Positive"]
+    return analyzer, labels
+
+# Makes 'app' a Python package (can be left empty)# Makes 'app' a Python package (can be left empty)
